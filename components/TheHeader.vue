af23224a150314e2d33778c7f96e6545cbd09bb2
caowei <solocao@126.com>
2018-08-10 15:04:13 +0800

    增加全局样式，在nuxt.config.js中设置，是支持stylus的，开心

---
+++ components/TheHeader.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <div class="top_menus">
+
+    </div>
+    <div class="logo_line">
+
+    </div>
+    <div class="container">
+
+    </div>
+  </div>
+</template>
+<script>
+import ShopCart from './ShopCart.vue'
+import ShopHeader from './ShopHeader.vue'
+import UserHoverCard from './UserHoverCard.vue'
+export default {
+  data() {
+    return {
+      showCart: false,
+      userCart: false
+    }
+  },
+  components: {
+    ShopCart,
+    ShopHeader,
+    UserHoverCard
+  },
+  computed: {
+    cartTotal() {
+      return this.$store.state.cartTotal;
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+@import '../style/var.styl';
+
+.navarea-shop {
+  height: 120px;
+
+  .capsule1 {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .title {
+      font-size: 18px;
+      font-weight: bold;
+    }
+  }
+
+  .shop-nav {
+    width: 100vw;
+    height: 60px;
+    background: white;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid linear-gradient(to right, #f3005c, #ffde00);
+  }
+
+  ul {
+    padding-left: 0;
+    display: flex;
+    list-style: none outside none;
+    justify-content: center;
+    align-items: center;
+  }
+
+  li {
+    padding: 0 50px;
+  }
+
+  a, a:visited, a:active {
+    text-decoration: none;
+    color: black;
+  }
+
+  .cartitem {
+    position: relative;
+    float: right;
+    margin-left: 15px;
+  }
+
+  .cartcount {
+    font-family: 'Barlow', sans-serif;
+    position: absolute;
+    background: #ff2211;
+    color: white;
+    text-align: center;
+    padding-top: 4px;
+    width: 20px;
+    height: 20px;
+    font-size: 10px;
+    margin: -5px 0 0 20px;
+    border-radius: 1000px;
+    font-weight: 700;
+  }
+}
+
+.slide-fade-enter-active {
+  transition: all 0.2s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(5px);
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-leave-active {
+  transition: all 10s ease;
+}
+
+.fade-enter, .fade-leave-to { /* .fade-leave-active below version 2.1.8 */
+  transform: translateX(5px);
+  opacity: 1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <main class="capsule" style="background:red">
+    <mast-head/>
+    <div class="contain">
+      <side-bar :pricerange.sync="highprice" />
+      <transition-group name="items" tag="section" class="content">
+        <shop-item v-for="(item) in products" :item="item" :key="item.id">
+        </shop-item>
+      </transition-group>
+    </div>
+  </main>
+</template>
+
+<script>
+import SideBar from '../components/SideBar.vue';
+import MastHead from '../components/MastHead.vue';
+import ShopItem from '../components/ShopItem.vue';
+
+export default {
+  layout: 'shop',
+  components: {
+    SideBar,
+    MastHead,
+    ShopItem
+  },
+  data() {
+    return {
+      highprice: 300,
+      products: []
+    };
+  },
+  computed: {
+    // products() {
+    //   return this.$store.state.products.filter(el =>
+    //     this.$store.state.sale
+    //       ? el.price < this.highprice && el.sale
+    //       : el.price < this.highprice
+    //   );
+    // }
+  },
+  methods: {
+    // 产品列表
+    async productList() {
+      const params = {
+        url: 'product/list',
+        payload: {}
+      }
+      const result = await this.post(params)
+      this.products = result.data
+      console.log(this.products)
+      console.log('看看结果')
+      console.log(result)
+    }
+  },
+  mounted() {
+    console.log('k俺看')
+    console.log(this.$store)
+
+    this.productList()
+
+    this.products1 = [{ 'name': 'Khaki Suede Polish Work Boots', 'price': 149.99, 'category': 'women', 'sale': true, 'article': 'shoe', 'img': 'shoe1.png' }, { 'name': 'Camo Fang Backpack Jungle', 'price': 39.99, 'category': 'women', 'sale': false, 'article': 'jacket', 'img': 'jacket1.png' }, { 'name': 'Parka and Quilted Liner Jacket', 'price': 49.99, 'category': 'men', 'sale': true, 'article': 'jacket', 'img': 'jacket2.png' }, { 'name': 'Cotton Black Cap', 'price': 12.99, 'category': 'men', 'sale': true, 'article': 'hats', 'img': 'hat1.png' }, { 'name': 'Knit Sweater with Zips', 'price': 29.99, 'category': 'women', 'sale': false, 'article': 'sweater', 'img': 'sweater1.png' }, { 'name': 'Long Linen-blend Shirt', 'price': 18.99, 'category': 'men', 'sale': false, 'article': 'shirt', 'img': 'shirt1.png' }, { 'name': 'Knit Orange Sweater', 'price': 28.99, 'category': 'men', 'sale': false, 'article': 'sweater', 'img': 'sweater2.png' }, { 'name': 'Cotton Band-collar Blouse', 'price': 49.99, 'category': 'men', 'sale': false, 'article': 'shirt', 'img': 'shirt2.png' }, { 'name': 'Camo Fang Backpack Jungle', 'price': 59.99, 'category': 'women', 'sale': true, 'article': 'jacket', 'img': 'jacket3.png' }, { 'name': 'Golden Pilot Jacket', 'price': 129.99, 'category': 'women', 'sale': false, 'article': 'jacket', 'img': 'jacket4.png' }, { 'name': 'Spotted Patterned Sweater', 'price': 80.99, 'category': 'women', 'sale': false, 'article': 'jacket', 'img': 'sweater4.png' }, { 'name': 'Double Knit Sweater', 'price': 59.99, 'category': 'men', 'sale': true, 'article': 'jacket', 'img': 'sweater5.png' }]
+    // this.productList()
+  }
+};
+</script>
+
+<style>
+/* no grid support */
+aside {
+  float: left;
+  width: 19.1489%;
+}
+
+.content {
+  /*no grid support*/
+  float: right;
+  width: 79.7872%;
+  /* grid */
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  padding: 0 !important;
+}
+</style>
